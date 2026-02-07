b00a1a26afcc383e86a996f6fefff3cfa2659c87
Ramprassath <ramprassathramsenthil@gmail.com>
2026-02-07 12:23:42 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-
-
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
-})
-*/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/Legal_Chat_Bot/',
+  plugins: [react()],
 })
